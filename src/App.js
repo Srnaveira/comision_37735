dc2764af3cb830f9b3ad582cdf6b296bb99ea223
Sebastian Naveira <srnaveire@gmail.com>
2022-05-14 01:33:39 -0300

    Realizando algunas modificaciones en el App.js y App.css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Practico en clase "CLASE-00"</h2>
+        <p class="Parrafo-PC">Crea un formulario de contacto utilizando HTML5, CSS y añade Js para simular su funcionamiento</p>
+        <div class="container__fields">
+        <label class="App-label">Ingrese su nombre</label>
+        <input type="text" class="App-inputs" required/>
+        </div>
+        <div class="container__fields">
+        <label class="App-label">Ingrese su Mail</label>
+        <input type="mail" class="App-inputs" required/>
+        </div>
+        <div class="container__fields">
+        <input type="submit" class="App-buttom" value="Enviar"/>       
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +28,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendiendo ReactJS
         </a>
       </header>
     </div>
